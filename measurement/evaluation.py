804f4d22e612d9c19b8bedc1861834533e6d1a39
jorgo <je051@hdm-stuttgart.de>
2017-05-03 08:46:28 -0400

    Various files renamed

---
+++ measurement/evaluation.py
@@ -0,0 +1,6 @@
+'''Contains all objects and functions regarding data evaluation'''
+
+class DataEvaluator(object):
+    '''Encapsulates all evaluation regarding the crawl-data from measuremnt'''
+    def __init__(self, realpart, imagpart):
+        pass
