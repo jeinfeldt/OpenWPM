8ea098d21655209c2e2b26b1f58403727ea9401f
jorgo <je051@hdm-stuttgart.de>
2017-05-03 08:34:51 -0400

    Restrutured parameters

--- measurement/analysis.py
+++ measurement/analysis.py
@@ -10,6 +10,40 @@ import sys, json, os, sqlite3
 HELP = '''Scripts needs to be executed with the following parameters:
 1. path to crawl-data (sqlite)\n(2. name for outputfile)'''
 
+def eval_first_party_cookies(connection):
+    '''Evaluates prevalence of first party cookies based on crawl data.
+       first-party: cookies set by top level domain'''
+    data = {}
+    qry = '''select site_url, baseDomain
+            from site_visits natural join profile_cookies;'''
+    cursor = connection.cursor()
+    # perform analysis
+    cursor.execute(qry)
+    for site_url, base_domain in cursor.fetchall():
+        main_domain = site_url.strip("http://www.")
+        if main_domain == base_domain:
+            amount = data.get(main_domain, 0)
+            data[main_domain] = amount + 1
+    data['total_sum'] = reduce(lambda x, y: x + y, data.values())
+    return data
+
+def eval_third_party_cookies(connection):
+    '''Evaluates prevalence of third party cookies based on crawl data.
+       third-party: cookies set outside of top level domain'''
+    data = {}
+    qry = '''select site_url, baseDomain
+            from site_visits natural join profile_cookies;'''
+    cursor = connection.cursor()
+    # perform analysis
+    cursor.execute(qry)
+    for site_url, base_domain in cursor.fetchall():
+        main_domain = site_url.strip("http://www.")
+        if main_domain != base_domain:
+            amount = data.get(main_domain, 0)
+            data[main_domain] = amount + 1
+    data['total_sum'] = reduce(lambda x, y: x + y, data.values())
+    return data
+
 def _write_result(data, output_path):
     '''writes all results to file'''
     pass
@@ -31,11 +65,16 @@ def _init():
 def _main():
     '''wrapper for main functionality'''
     db_path, output = _init()
+    connection = sqlite3.connect(db_path)
     print "Starting analysis..."
+    data = eval_first_party_cookies(connection)
+    data = eval_third_party_cookies(connection)
     if output is not None:
         print "Finished analysis, data written to %s" %(output)
     else:
         print "Finished analysis, here is the data:"
+        print data['total_sum']
+    connection.close()
 #main
 if __name__ == "__main__":
     _main()
