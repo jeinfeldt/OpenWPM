5a29db16ec7dda26075642bb955d50bc72e3c364
Jorgo <je051@hdm-stuttgart.de>
2017-06-20 18:02:52 +0200

    Adjsuted crawler for new site input

--- crawl.py
+++ crawl.py
@@ -6,12 +6,11 @@ from time import gmtime, strftime
 from automation import TaskManager, CommandSequence
 
 # constants
-WEBSITE_FILE = 'measurement/assets/quantcast-top-million.txt'
-HELP = '''Scripts needs to be executed with the following parameters:
+HELP = '''\nScripts needs to be executed with the following parameters:
 1. path to browser parameter .json-file\n2. path to manager parameter .json-file
-3. number of pages to crawl\nHint: For better results perform script as sudo'''
+3. number of pages to crawl\n4. site input file (alexa list)
+(5. Prefix of output db)\nHint: For better results perform script as sudo\n'''
 
-# public
 def crawl(sites, browser_params, manager_params):
     '''crawls given sites with given parameters'''
     # Shuts down the browsers and waits for the data to finish logging
@@ -26,7 +25,7 @@ def _crawl_analysis(sites, browser_params, manager_params):
         # we run a stateless crawl (fresh profile for each page)
         command_sequence = CommandSequence.CommandSequence(site, reset=True)
         # Start by visiting the page
-        command_sequence.get(sleep=15, timeout=60)
+        command_sequence.get(sleep=15, timeout=30)
         # dump_profile_cookies/dump_flash_cookies closes the current tab.
         command_sequence.dump_profile_cookies(120)
         command_sequence.dump_flash_cookies(120)
@@ -37,27 +36,23 @@ def _crawl_detection(sites, browser_params, manager_params):
     '''Runs crawl resulting in dataset for unsupervised tracking detection'''
     num_visits, num_users = manager_params['num_visits'], manager_params['num_users']
     browser_params['disable_flash'] = True
-    # TODO: just for testing
-    manager_params['database_name'] = "05-vanilla-100-detection-crawl-data.sqlite"
-    manager = TaskManager.TaskManager(manager_params, [browser_params])
-    for site in sites:
-        for _ in range(0, num_visits):
-            command_sequence = CommandSequence.CommandSequence(site)
-            command_sequence.get(sleep=30, timeout=60)
-            manager.execute_command_sequence(command_sequence, index='**')
-    manager.close()
+    for _ in range(0, num_users):
+        manager = TaskManager.TaskManager(manager_params, [browser_params])
+        for site in sites:
+            for _ in range(0, num_visits):
+                command_sequence = CommandSequence.CommandSequence(site)
+                command_sequence.get(sleep=15, timeout=30)
+                manager.execute_command_sequence(command_sequence, index='**')
+        manager.close()
 
 def load_websites(file_path, amount):
-    '''loads defined amount of pages from file, websites are returned
-       in format http://www.[domainname].[identifier]
-       Currently only for quantcast files'''
+    '''loads defined amount of pages from alexa file, websites are returned
+       in format http://www.[domainname].[identifier]'''
     url_format = 'http://www.%s'
-    filter_string = 'Hidden profile'
     with open(file_path, 'r') as data:
-        # specific parsing to quantcast format
-        sites = [row.strip() for row in list(data) if row[0].isdigit()]
-        sites = [row for row in sites if filter_string not in row]
-        sites = [url_format %(row.split("\t")[1]) for row in sites]
+        sites = [line.strip() for line in data if "#" not in line]
+        sites = [line for line in sites if line] # remove blank
+        sites = [url_format %(line) for line in sites]
         return sites[:amount]
 
 def load_parameters(file_path):
@@ -75,23 +70,26 @@ def generate_crawl_prefix(browser_params_path, manager_params, amount):
 def _init():
     '''guard clause and init for script'''
     args = sys.argv[1:]
-    if len(args) != 3:
+    if len(args) < 4:
         print HELP
         sys.exit()
+    if len(args) == 4:
+        args.append(None)
     return args
 
 def _main():
     '''wrapper for main functionality'''
-    browser_path, manager_path, amount = _init()
+    browser_path, manager_path, amount, sites_path, db_prefix = _init()
     # perform crawl
     print 'Preparing crawl...'
     browser_params = load_parameters(browser_path)
     manager_params = load_parameters(manager_path)
     # better identifiable names for log and db
-    prefix = generate_crawl_prefix(browser_path, manager_params, amount)
+    generated = generate_crawl_prefix(browser_path, manager_params, amount)
+    prefix = db_prefix if db_prefix is not None else generated
     manager_params['database_name'] = prefix + manager_params['database_name']
     manager_params['log_file'] = prefix + manager_params['log_file']
-    websites = load_websites(WEBSITE_FILE, int(amount))
+    websites = load_websites(sites_path, int(amount))
     print 'Crawling...'
     crawl(websites, browser_params, manager_params)
     print 'Finished crawling, data written to: %s' %(manager_params['database_name'])
